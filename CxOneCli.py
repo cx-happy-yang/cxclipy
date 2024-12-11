e91426156f4a89bca151c15f75e5fe55a2bc8407
unknown <30431255+HappyY19@users.noreply.github.com>
2024-12-11 14:06:59 +0800

    [REMOVE] comment

--- CxOneCli.py
+++ CxOneCli.py
@@ -1,11 +1,3 @@
-"""
-This a CLI script. It can be converted to binary file by using pyinstaller
-
-pyinstaller -y -F --clean CxOneCli.py
-
-scan --cxone_access_control_url https://eu.iam.checkmarx.net --cxone_server https://eu.ast.checkmarx.net --cxone_tenant_name asean_2021_08 --cxone_grant_type refresh_token --cxone_refresh_token "****" --preset "ASA Premium"  --incremental false --location_path /home/happy/Documents/SourceCode/github/java/JavaVulnerableLab --project_name happy-test-2022-04-20 --branch master --exclude_folders "test,integrationtest" --exclude_files "*min.js" --report_csv cx-report.csv --full_scan_cycle 10 --cxone_proxy http://127.0.0.1:1081
-
-"""
 import hashlib
 import pathlib
 import time
