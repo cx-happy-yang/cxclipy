056f02c52815c0af59d9862f9fd15328c7b37e28
unknown <30431255+HappyY19@users.noreply.github.com>
2024-12-13 14:12:13 +0800

    [UPDATE] run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 curl -LO https://github.com/HappyY19/cxclipy/releases/download/v0.3.3/CxOneCli
 chmod +x ./CxOneCli
 source ~/.secrets
+# run sca scan
 ./CxOneCli scan \
 --cxone_access_control_url https://sng.iam.checkmarx.net \
 --cxone_server https://sng.ast.checkmarx.net \
@@ -22,4 +23,28 @@ source ~/.secrets
 --sca_exploitable_path true \
 --branch master-sca \
 --scanners sca \
+--sca_last_sast_scan_time 2
+
+# run other scans
+./CxOneCli scan \
+--cxone_access_control_url https://sng.iam.checkmarx.net \
+--cxone_server https://sng.ast.checkmarx.net \
+--cxone_tenant_name happy  \
+--cxone_grant_type refresh_token \
+--cxone_refresh_token $CXONE_HAPPY_TOKEN \
+--preset "ASA Premium"  \
+--incremental false \
+--location_path /mnt/e/github.com/CSPF-Founder/JavaVulnerableLab \
+--project_name AlphaTeam/JavaVulnerableLab \
+--exclude_folders "test,integrationtest" \
+--exclude_files "*min.js" \
+--report_csv cx-report.csv \
+--full_scan_cycle 10  \
+--scan_tag_key key1,key2 \
+--scan_tag_value value1,value2 \
+--parallel_scan_cancel true \
+--scan_commit_number 1 \
+--sca_exploitable_path true \
+--branch master \
+--scanners sast,apisec,kics,containers,microengines \
 --sca_last_sast_scan_time 2
