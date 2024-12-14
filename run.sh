5b9f04659b78f778814ad968b7f1aa353f640d6a
unknown <30431255+HappyY19@users.noreply.github.com>
2024-12-14 14:42:59 +0800

    [UPDATE] version 0.3.5
    Signed-off-by: unknown <30431255+HappyY19@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-curl -LO https://github.com/HappyY19/cxclipy/releases/download/v0.3.4/CxOneCli
+curl -LO https://github.com/HappyY19/cxclipy/releases/download/v0.3.5/CxOneCli
 chmod +x ./CxOneCli
 source ~/.secrets
 # run sca scan
