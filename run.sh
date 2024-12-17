a9df73965d9115ecda96a710fdc37364680ed79a
unknown <30431255+HappyY19@users.noreply.github.com>
2024-12-17 20:04:10 +0800

    version 0.3.9

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-curl -LO https://github.com/HappyY19/cxclipy/releases/download/v0.3.8/CxOneCli
+curl -LO https://github.com/HappyY19/cxclipy/releases/download/v0.3.9/CxOneCli
 chmod +x ./CxOneCli
 source ~/.secrets
 # run sca scan
