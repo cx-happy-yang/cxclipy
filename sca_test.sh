2d8f9753385c3c543e554f300a97bfea004efa02
cx-happy-yang <30431255+cx-happy-yang@users.noreply.github.com>
2025-08-25 12:14:25 +0800

    version 0.5.7

--- sca_test.sh
+++ sca_test.sh
@@ -1,4 +1,4 @@
-curl -LO https://github.com/HappyY19/cxclipy/releases/download/v0.5.6/CxOneCli
+curl -LO https://github.com/HappyY19/cxclipy/releases/download/v0.5.7/CxOneCli
 chmod +x ./CxOneCli
 source ~/.secrets
 # run sca scan
